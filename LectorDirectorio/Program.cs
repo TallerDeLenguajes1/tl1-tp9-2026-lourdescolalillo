7100328f51095355f959a0f2280dbbc16d13fcae
lourdes colalillo <lourdescolalillo@gmail.com>
2026-06-10 15:19:12 -0300

    ejercicio 1 apartados a y b

---
+++ LectorDirectorio/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+class Program
+{
+    static void Main()
+    {
+        string ruta = " ";
+        bool rutaValida = false;
+        do
+        {
+            Console.Write("Ingrese el path del directorio que desea analizar: ");
+            ruta = Console.ReadLine();
+            if (Directory.Exists(ruta))
+            {
+                rutaValida = true;
+            }
+            else
+            {
+                Console.WriteLine("Ingrese un path valido");
+            }
+        } while (!rutaValida);
+    }
+}
+
